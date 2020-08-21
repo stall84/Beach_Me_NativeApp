032fce7c164866ad2e5093815f1e7b577a4a5f90
Michael Stallings <stall84@gmail.com>
2020-08-21 08:45:23 -0400

    Initial Commit. Got working App component with geolocation - cooridinate finding functions and state built

---
+++ App.js
@@ -0,0 +1,63 @@
+import 'react-native-gesture-handler';
+import React, {useState, useEffect, useCallback} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Geolocation from '@react-native-community/geolocation';
+
+const App = () => {
+  const [coords, setCoords] = useState({lat: '', lng: ''});
+
+  const getCoords = useCallback(() => {
+    Geolocation.getCurrentPosition((position) => {
+      console.log('Geolocation: ', position);
+      setCoords({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Beach-Go'ers!</Text>
+      <TouchableOpacity style={styles.button} onPress={getCoords}>
+        <Text>Press To Get Beached</Text>
+      </TouchableOpacity>
+      <View>
+        <Text style={styles.stateDisp}>
+          Lat: {coords.lat}
+          Lng: {coords.lng}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 40,
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#87CEEB',
+    marginTop: 15,
+    padding: 15,
+    borderRadius: 10,
+  },
+  stateDisp: {
+    marginTop: 80,
+    padding: 20,
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
