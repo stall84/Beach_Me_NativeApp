46d21194acfa8a8c2bcef186111cd8e9c345ba37
Michael Stallings <stall84@gmail.com>
2020-08-21 21:28:19 -0400

    Added axios calls to beach-me-backend api returning good database beaches via callbacks, stored in hooks. Starting to work on Display screen and backend calls to Google distance matrix api

--- screens/Display.js
+++ screens/Display.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Display = () => {
+const Display = ({route}) => {
+  const [searchBeaches, setSearchBeaches] = useState({searchBeaches: ''});
+  const {Lat, Lng} = route.params.userCoords;
+
   return (
     <View style={styles.container}>
       <Text>THIS WILL BE THE DISPLAY PAGE</Text>
+      <Text>
+        Route Param-Props: Lat: {Lat}, Lng: {Lng}
+      </Text>
     </View>
   );
 };
